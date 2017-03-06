b8c39aedd3a21501cc5669d44417fe936ef50c51
shmakovdima <contact@shmakovdima.ru>
2017-03-06 19:19:41 +0300

    previous variant

--- src/components/CardBar.js
+++ src/components/CardBar.js
@@ -11,19 +11,15 @@ import {
   Image,
   PanResponder,
   Animated,
-  Dimensions,
-  Text
+  Dimensions
 } from 'react-native'
 
+let dataCards = []
 
-let dataCards = [
-
-]
-
-for (let i = 0; i <= 5; i++) {
+for (let i = 0; i <= 20; i++) {
   dataCards[i] = {
-    type: 'Rub',
-    value: 1234
+    type: 'USD',
+    value: '4000'
   }
 }
 
@@ -111,7 +107,7 @@ export default class PurchasesBar extends Component {
     const step = this.state.dY
 
     let heightViewport = {
-      height: win.width / 1.6 + 70 + ((step < 0) ? step * 1.8 : step)
+      height: win.width / 1.6 + 70 + ((step < 0) ? step * 1.8 : 3 * step)
     }
     return (
       <Animated.View style={[ Style.cardBar, heightViewport ]} {...this._panResponder.panHandlers}>
@@ -135,7 +131,6 @@ class CardImage extends Component {
 
     this._totalUnits = this.props.total
     this._position = this.props.position
-
     this._inversionPosition = this._totalUnits - this._position - 1
 
     let startPosition = 0
@@ -184,17 +179,18 @@ class CardImage extends Component {
 
     this.state = {
       verticalPosition: startPosition,
-      imaginalVerticalPosition: startPosition,
+      startVerticalPosition: startPosition,
       rotate: startRotate,
+      startRotate: startRotate,
       opacity: startOpacity,
+      startOpacity: startOpacity,
       rotateSecond: startRotateSecond,
+      startRotateSecond: startRotateSecond,
       scaleX: startScaleX,
+      startScaleX: startScaleX,
       inversionPosition: this._inversionPosition,
       previousStep: 0
     }
-  }
-  _setVerticalPosition (step) {
-
   }
 
   componentWillReceiveProps () {
@@ -204,13 +200,17 @@ class CardImage extends Component {
     const previousRotate = this.state.rotate
     const previousOpacity = this.state.opacity
     const previousRotateSecond = this.state.rotateSecond
+    const total = this.props.total
     const previousScaleX = this.state.scaleX
     const inversionPosition = this.state.inversionPosition
-    const startPosition = this.state.startPosition
-    let imaginalVerticalPosition = this.state.imaginalVerticalPosition
-
+    const startRotate = this.state.startRotate
+    const startRotateSecond = this.state.startRotateSecond
+    const startVerticalPosition = this.state.startVerticalPosition
+    const startOpacity = this.state.startOpacity
+    const startScaleX = this.state.startScaleX
     const changeY = step - previousStep
-
+    const heightViewport = win.height - win.width / 1.6
+    const newVerticalStep = step - 100 * (inversionPosition - 3)
 
     let newVerticalPosition = previousVertivalPosition
     let newRotate = previousRotate
@@ -218,53 +218,94 @@ class CardImage extends Component {
     let newRotateSecond = previousRotateSecond
     let newScaleX = previousScaleX
 
-    if (inversionPosition === 4) {
-      console.log(previousVertivalPosition + ' ' + imaginalVerticalPosition);
-    }
-
-
     if (step <= 0) {
       if (inversionPosition === 0) {
-        newVerticalPosition = imaginalVerticalPosition + step * 2
+        newVerticalPosition = startVerticalPosition + step * 1.4
       } else if (inversionPosition === 1) {
-        newVerticalPosition = imaginalVerticalPosition + step * 1.5
+        newVerticalPosition = startVerticalPosition + step * 1.8
       } else {
-        newVerticalPosition = imaginalVerticalPosition + step
+        newVerticalPosition = startVerticalPosition + step
       }
-
 
       if (newVerticalPosition <= 0) {
         newVerticalPosition = 0
       }
     } else {
-      newVerticalPosition = newVerticalPosition + changeY
+
+      if (inversionPosition === 0) {
+        newVerticalPosition = startVerticalPosition + step * 3
+      }
+
+      if (inversionPosition === 1) {
+        if (newVerticalPosition < heightViewport * 0.75) {
+          newVerticalPosition = startVerticalPosition + step * 2.32
+        } else {
+          newVerticalPosition = heightViewport * 0.75 + (step - (heightViewport * 0.75 - startVerticalPosition) / 2.32) * 3
+        }
+      }
+      if (inversionPosition === 2) {
+        if (newVerticalPosition < heightViewport * 0.4) {
+          newVerticalPosition = startVerticalPosition + step * 1.43
+        } else {
+          newVerticalPosition = heightViewport * 0.4 + (step - (heightViewport * 0.4 - startVerticalPosition) / 1.43) * 3.75
+        }
+      }
+
+      if (inversionPosition > 2) {
+        if (newVerticalStep > 0) {
+          newVerticalPosition = startVerticalPosition + newVerticalStep * 0.5
+          if (previousVertivalPosition >= 0.1 * heightViewport) {
+            newVerticalPosition = startVerticalPosition + newVerticalStep * 1
+          }
+        }
+      }
+
+      const proportion = newVerticalPosition / heightViewport
+
+      newScaleX = startScaleX + ((newVerticalPosition - startVerticalPosition) / (heightViewport * 0.2)) * (1 - startScaleX)
+
+      if (inversionPosition === 0) {
+        newRotate = startRotate + proportion * (95 - startRotate)
+        newRotateSecond = startRotateSecond + proportion * proportion * (40 - startRotateSecond)
+      }
+
+      if (inversionPosition === 1) {
+        newRotate = startRotate + proportion * (90 - startRotate)
+        newRotateSecond = startRotateSecond + proportion * (35 - startRotateSecond)
+      }
+      if (inversionPosition === 2) {
+        newRotate = startRotate + proportion * (90 - startRotate)
+        newRotateSecond = startRotateSecond + proportion * (35 - startRotateSecond)
+      }
+
+      if (inversionPosition > 2) {
+        newRotate = startRotate + proportion * (90 - startRotate)
+        newRotateSecond = startRotateSecond + proportion * (35 - startRotateSecond)
+      }
+
+      newOpacity = startOpacity + ((newVerticalPosition - startVerticalPosition) / (heightViewport * 0.4)) * (1 - startOpacity)
+
+      if (inversionPosition > 1) {
+        newOpacity = startOpacity + ((newVerticalPosition - startVerticalPosition) / (heightViewport * 0.2)) * (1 - startOpacity)
+      }
+
+      if (newScaleX >= 1) {
+        newScaleX = 1
+      }
+
+      if (newOpacity >= 1) {
+        newOpacity = 1
+      }
     }
-
-
-
 
     this.setState({
       previousStep: step,
       verticalPosition: newVerticalPosition,
-      imaginalVerticalPosition: imaginalVerticalPosition,
       rotate: newRotate,
       opacity: newOpacity,
       scaleX: newScaleX,
       rotateSecond: newRotateSecond
     })
-
-/*
-
-      // While step < 0 (inertial to top position)
-    if (step <= 0) {
-
-        }
-      } else {
-
-        //stepVerticalPosition = this._setVerticalPosition(step, dy, this.state.dY, inversionPosition)
-      }
-*/
-
   }
 
   render () {
@@ -280,11 +321,11 @@ class CardImage extends Component {
       opacity: stepOpacity
     }
 
-    const stepAddView = {transform: [{ rotateX: 120 - stepRotate + 'deg'}]}
+    const stepAddView = {transform: [{rotateX: 120 - stepRotate + 'deg'}]}
 
     return (
       <View style={[Style.cardBar__item, stepBlock]}>
-        <TouchableHighlight style={Style.cardBar__press}  onPress={() => Actions.menucart()} underlayColor='transparent' activeOpacity={1}>
+        <TouchableHighlight style={Style.cardBar__press} onPress={() => Actions.menucart()} underlayColor='transparent' activeOpacity={1}>
           <Image style={Style.cardBar__item__image} source={require('../../images/card.png')}/>
         </TouchableHighlight>
         <View style={[Style.cardBar__item__imageAddings, stepAddView]}></View>
